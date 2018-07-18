a1f848ec37c876a17260d0c79256c2cdcbe3b988
Olena-Brotman <alyona.mor@gmail.com>
2018-07-18 23:24:01 +0200

    2nd stage, sold button

--- app/src/main/java/com/example/android/inventory/CatalogActivity.java
+++ app/src/main/java/com/example/android/inventory/CatalogActivity.java
@@ -15,8 +15,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -36,10 +39,15 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
     //Adapter for the list view
     ProductCursorAdapter mCursorAdapter;
 
+    //EditText field to enter the quantity of the product
+    private EditText mProductQuantityEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        mProductQuantityEditText = (EditText) findViewById(R.id.quantity);
 
         //Setup FAB to open the EditorActivity
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -80,6 +88,23 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
         getLoaderManager().initLoader(PRODUCT_LOADER, null, this);
     }
 
+    //Sold button decreases the given quantity of the products by 1
+    Button buttonSold = (Button) findViewById(R.id.button_sold);
+        buttonSold.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(Loader<Cursor> loader, Cursor cursor){
+            if (cursor == null || cursor.getCount() < 1) {
+                return;
+            }
+            if (cursor.moveToFirst()){
+                int quantitySum = cursor.getColumnIndex(ProductContract.ProductEntry.COLUMN_INV_QUANTITY);
+                int quantity = cursor.getInt(quantitySum);
+                quantity = quantity - 1;
+                display(quantity);
+            }
+
+        });
+
     //Helper method to insert hard coded data into the database, for debugging purposes only
     private void insertProduct() {
         //Create a ContentValues object where column names are the keys, and
@@ -88,7 +113,7 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
         values.put(ProductContract.ProductEntry.COLUMN_INV_NAME, "Inferno");
         values.put(ProductContract.ProductEntry.COLUMN_INV_PRICE, 600);
         values.put(ProductContract.ProductEntry.COLUMN_INV_QUANTITY, 10);
-        values.put(ProductContract.ProductEntry.COLUMN_INV_SUPPLIER, ProductContract.ProductEntry.UNKNOWN_SUPPLIER);
+        values.put(ProductContract.ProductEntry.COLUMN_INV_SUPPLIER, ProductContract.ProductEntry.COMPANY_1);
         values.put(ProductContract.ProductEntry.COLUMN_INV_SUPPLIER_PHONE, 0305550330);
 
         Uri newUri = getContentResolver().insert(ProductContract.ProductEntry.CONTENT_URI, values);
@@ -118,6 +143,7 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
+                deleteAllProducts();
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -151,4 +177,12 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
         //Callback is called when the data is needed to be deleted
         mCursorAdapter.swapCursor(null);
     }
-}
+
+    /**
+     * This method displays the given quantity of the products
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_item);
+        quantityTextView.setText("" + number);
+    }
+  }
